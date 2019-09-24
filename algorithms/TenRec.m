7da21ec29499adc44c3e523437c94e0f9eb7a63d
Charilaos Kanatsoulis <kanat003@umn.edu>
2019-09-24 12:51:35 -0400

    Update TenRec.m

--- algorithms/TenRec.m
+++ algorithms/TenRec.m
@@ -1,7 +1,7 @@
 function[A_hat,B_hat,C_hat,A_tilde,B_tilde,C_tilde]=TenRec(MSI,H3,maxit,t_rank,P1,P2) 
 % Ten-Rec algorithm
 % (c) Charilaos I. Kanatsoulis, University of Minnesota, Jan 7 , 2018
-% nikos@umn.edu
+% kanat003@umn.edu
 % 
 % Reference 1: C.I. Kanatsoulis, X. Fu, N.D. Sidiropoulos and W.K. Ma, 
 %``Hyperspectral Super-resolution: A Coupled Tensor Factorization
